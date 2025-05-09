20ed7ee33f82ef89ad73e3e5a33487fee4b2be52
村田直人 <aa700100aa@gmail.com>
2025-05-10 07:09:32 +0900

    ✨:Add:React + TypeScript + Prettier + ESLint の初期設定

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <main>
-      <h1>Movie Search App</h1>
-    </main>
-  );
-}
-
-export default App;
---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App(): JSX.Element {
+  const msg = 'こんにちは';
+  return (
+    <div>
+      <h1>{msg}</h1>
+    </div>
+  );
+}
+
+export default App;
